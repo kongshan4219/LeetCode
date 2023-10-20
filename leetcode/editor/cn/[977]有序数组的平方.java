0cc7b3f94d51e7de0890df8eeb34ecb2287c7b03
空山 <2996014561@qq.com>
2023-10-20 16:05:02 +0800

    数组：977.有序数组的平方 1、for循环计算后排序（等待优化） 2、用双指针直接在原数组修改，但是没写出来。。。 3、新建一个数组，双指针选大的存入，简简单单

--- leetcode/editor/cn/[977]有序数组的平方.java
+++ leetcode/editor/cn/[977]有序数组的平方.java
@@ -47,12 +47,66 @@ class Solution {
         int left = 0;
         int right = nums.length-1;
 
-        //for循环计算后排序
-        for(int i=0;i<nums.length;i++){
-            nums[i] = nums[i]*nums[i];
+        int[] res = new int[nums.length];
+        int idx = nums.length-1;
+        while (left<=right){
+            if ( nums[left]*nums[left]>nums[right]*nums[right]){
+                res[idx] = nums[left]*nums[left];
+                left++;
+                idx--;
+            }else {
+                res[idx] = nums[right]*nums[right];
+                right--;
+                idx--;
+            }
         }
-        Arrays.sort(nums);
-        return nums;
+        return res;
+
+        //while (left<=right){
+        //    //比较左右边界 例子：初始 [-9,-4,0,3,10]
+        //    //[-9,-4,0,3,100].
+        //    //[3,-4,0,-9,100]
+        //    if (nums[left]*nums[left]>nums[right]*nums[right]){
+        //        //左边界大于右边界，但还不能确定左边界的平方是不是最大的，[3,-4,0,81,100]
+        //        //因为此时左边界不一定是原始的左边界，可能是从右边界交换过来的，像这样[3,-4,0,81,100]
+        //        if (nums[left]*nums[right]>0){
+        //            //此时,可以确定 左方 大于 右方，且 左方 为当前最大值
+        //            int tmp = nums[right];
+        //            nums[right] = nums[left]*nums[left];
+        //            nums[left] = tmp;
+        //            right--;
+        //        }else {
+        //            //此时， 不确定 左方 是不是最大
+        //            int tmp = nums[right];
+        //            nums[right] = nums[left];
+        //            //nums[left] = tmp*tmp;
+        //            //left++;
+        //        }
+        //    }else {
+        //        //[3,-4,0,-9,100]
+        //       //左边界方小于右边界方， 但 左方 不一定是最小，但右方确定最大
+        //        if (nums[left]*nums[right]<0){
+        //            nums[right] = nums[right]*nums[right];
+        //            right--;
+        //            //此时 左方 最小
+        //            //nums[left] = nums[left]*nums[left];
+        //            //left++;
+        //        }else {
+        //            //nums[right] = nums[right]*nums[right];
+        //            //right--;
+        //            nums[left] = nums[left]*nums[left];
+        //            left++;
+        //        }
+        //    }
+        //}
+        //return nums;
+
+        //for循环计算后排序
+        //for(int i=0;i<nums.length;i++){
+        //    nums[i] = nums[i]*nums[i];
+        //}
+        //Arrays.sort(nums);
+        //return nums;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
